ec0c269787cee808c7aa855cfbd8c979c0b03778
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-15 23:48:11 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] aryNums;
+        aryNums = new int[6];
+
+        aryNums[0] = 10;
+        aryNums[1] = 37;
+        aryNums[2] = 65;
+        aryNums[3] = 40;
+        aryNums[4] = 16;
+        aryNums[5] = 18;
+
+	// write your code here
+    }
+}
